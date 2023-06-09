dcf5ef64851b137e01325c2cabfde9ca5c3ffb67
Antonio Cuni <anto.cuni@gmail.com>
2023-06-09 17:51:42 +0200

    ooops

--- proof-of-concept/pyberryscript.py
+++ proof-of-concept/pyberryscript.py
@@ -4,5 +4,5 @@ pyscript_sync.monkey_patch()
 
 def setup(address, port):
     os.environ['GPIOZERO_PIN_FACTORY'] = 'pigpio'
-    os.environ['PIGPIO_ADDR'] = '192.168.178.148'
-    os.environ['PIGPIO_PORT'] = '1234'
+    os.environ['PIGPIO_ADDR'] = address
+    os.environ['PIGPIO_PORT'] = port
