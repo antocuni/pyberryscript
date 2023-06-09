48cc822ce30e6ee54fae14d9b12b64b46cc742fe
Antonio Cuni <anto.cuni@gmail.com>
2023-06-09 14:36:38 +0200

    another demo

--- proof-of-concept/gpiozero_local_demo.py
+++ proof-of-concept/gpiozero_local_demo.py
@@ -1,20 +1,42 @@
 import os
-from gpiozero import LED
-from time import sleep
+from gpiozero import LED, PWMLED
+import time
 
 os.environ['PIGPIO_ADDR'] = '192.168.178.148'
 
-red = LED(18)
-green = LED(23)
+def blink():
+    red = LED(18)
+    green = LED(23)
 
-try:
-    while True:
-        red.on()
-        green.off()
-        sleep(0.5)
+    try:
+        while True:
+            red.on()
+            green.off()
+            time.sleep(0.5)
+            red.off()
+            green.on()
+            time.sleep(0.5)
+    except KeyboardInterrupt:
         red.off()
-        green.on()
-        sleep(0.5)
-except KeyboardInterrupt:
-    red.off()
-    green.off()
+        green.off()
+
+
+def pulse():
+    red = PWMLED(18)
+    red.value = 0.5
+    step = 0.1
+    while True:
+        new_value = red.value + step
+        if new_value <= 0.0:
+            new_value = 0.0
+            step = -step
+        if new_value >= 1.0:
+            new_value = 1.0
+            step = -step
+        red.value = new_value
+        time.sleep(0.01)
+
+
+if __name__ == '__main__':
+    #blink()
+    pulse()
