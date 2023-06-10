a8e07afd14953909b6d6de077766ef58b4fc8129
Antonio Cuni <anto.cuni@gmail.com>
2023-06-10 16:04:33 +0200

    improve this demo

--- proof-of-concept/toggle_led.py
+++ proof-of-concept/toggle_led.py
@@ -5,16 +5,17 @@ import pigpio
 
 RED = 18
 #GREEN = 23
+BUILTIN = 47  # this is the builtin led of the pi zero
 
 print('connect')
 pi = pigpio.pi('192.168.178.148')
 
-print('LED ON')
-pi.write(RED, 1)
-
-time.sleep(0.5)
-
-print('LED OFF')
-pi.write(RED, 0)
-
-print('bye')
+try:
+    while True:
+        pi.write(RED, 1)
+        time.sleep(0.5)
+        pi.write(RED, 0)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    print()
+    pass
